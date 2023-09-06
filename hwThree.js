dacc4893ee895e979c655287f5210ea64068bc10
kaw17 <kaw17@iu.edu>
2023-09-06 16:55:07 -0400

    first

---
+++ hwThree.js
@@ -0,0 +1,9 @@
+var prin=parseFloat(document.getElementById('principle').value);
+var intRate=parseFloat(document.getElementById('rate').value);
+var ratePerc=intRate/100;
+var varTime=parseFloat(document.getElementById('time').value);
+var halfAnEquation=1+ratePerc*varTime
+
+function calculate(){
+    document.getElementById("changeTxt").innerHTML="With a starting principle of $" + prin + " and with a growth rate of " + intRate + "% for " + varTime + " years, your total interest will be " + prin*halfAnEquation;
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+# Kevin Willis
+## 8/30/23
+### Assignment Three
+I'm submitting this per your request since my website displays NaN rather than the number despite using parseFloat
+Weblink: https://in-info-web4.informatics.iupui.edu/~kaw17/homework_three/homeworkThree.html
