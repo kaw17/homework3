8e8c699504b3afd0e96e19e95e9f9e136f3f8834
kaw17 <kaw17@iu.edu>
2023-09-11 14:53:07 -0400

    working

--- hwThree.js
+++ hwThree.js
@@ -1,9 +1,10 @@
-var prin=parseFloat(document.getElementById('principle').value);
-var intRate=parseFloat(document.getElementById('rate').value);
-var ratePerc=intRate/100;
-var varTime=parseFloat(document.getElementById('time').value);
-var halfAnEquation=1+ratePerc*varTime
 
 function calculate(){
-    document.getElementById("changeTxt").innerHTML="With a starting principle of $" + prin + " and with a growth rate of " + intRate + "% for " + varTime + " years, your total interest will be " + prin*halfAnEquation;
+    var prin=parseFloat(document.getElementById('principle').value);
+    var intRate=parseFloat(document.getElementById('rate').value);
+    var ratePerc=intRate/100;
+    var varTime=parseFloat(document.getElementById('time').value);
+    var halfAnEquation=1+ratePerc*varTime
+
+    document.getElementById("changeTxt").innerHTML="With a starting principle of $" + prin + " and with a growth rate of " + intRate + "% for " + varTime + " years, your total interest will be " + prin*halfAnEquation.toFixed(2);
 }
